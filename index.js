4245483eab7065f80fbb94194960e314f93de28d
Andrew Walker <hovercar00@gmail.com>
2017-12-21 04:05:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function deleteFromObjectByKey(object, key) {
   return newObj;
 }
 
-function destructivelyDeleteFrom ObjectByKey(object, key) {
-  
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
 }
