d50d8318f39e85f0b8954dac73cdec908f493b08
Andrew Walker <hovercar00@gmail.com>
2017-12-21 04:03:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, key)
-  return newObj
+  var newObj = Object.assign({}, key);
+  return newObj;
 }
